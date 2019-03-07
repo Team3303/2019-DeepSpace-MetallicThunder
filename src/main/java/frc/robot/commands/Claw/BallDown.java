f6442e581f60f49fd3557999dd6ce329045d8863
Madsen Altamirano <1supersonicfan@gmail.com>
2019-03-07 13:49:20 -0800

    Fix BallDown to prevent possibility of not running at all

--- src/main/java/frc/robot/commands/Claw/BallDown.java
+++ src/main/java/frc/robot/commands/Claw/BallDown.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands.Claw;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -24,14 +17,12 @@ public class BallDown extends Command {
 
   // Called repeatedly when this Command is scheduled to run
   @Override
-  protected void execute() {
-    //Robot.claw.clawDown();
-  }
+  protected void execute() { }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return true;
+    return false;
   }
 
   // Called once after isFinished returns true
