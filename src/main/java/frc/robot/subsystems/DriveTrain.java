172ab5cb4a734c30d0d919cfab96f52569ffd6a6
Madsen Altamirano <1supersonicfan@gmail.com>
2019-03-08 08:34:15 -0800

    Switch drivetrain to talons

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -14,46 +14,46 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import frc.robot.RobotMap;
-import frc.robot.Robot;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import frc.robot.Robot;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
 /**
- * Add your docs here.
+ * Subsystem for the Robot's drive train
  */
 public class DriveTrain extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-  WPI_VictorSPX talon_FL;
-  WPI_VictorSPX talon_FR;
-  WPI_TalonSRX talon_BL;
-  WPI_TalonSRX talon_BR;
-  PigeonIMU pigeon;
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+//  WPI_VictorSPX talon_FL;
+//  WPI_VictorSPX talon_FR;
+	WPI_TalonSRX talon_FL;
+	WPI_TalonSRX talon_FR;
+	WPI_TalonSRX talon_BL;
+	WPI_TalonSRX talon_BR;
+	PigeonIMU pigeon;
 
-  DifferentialDrive differentialDrive;
-  
-  public DriveTrain() {
-    talon_FL = RobotMap.talon_FL;
-    talon_FR = RobotMap.talon_FR;
-    talon_BL = RobotMap.talon_BL;
-    talon_BR = RobotMap.talon_BR;
-    pigeon = RobotMap.pigeon;
-    differentialDrive = new DifferentialDrive(talon_FL, talon_FR);
+	DifferentialDrive differentialDrive;
 
-    talon_BL.follow(talon_FL);
-    talon_BR.follow(talon_FR);
-  }
+	public DriveTrain() {
+		talon_FL = RobotMap.talon_FL;
+		talon_FR = RobotMap.talon_FR;
+		talon_BL = RobotMap.talon_BL;
+		talon_BR = RobotMap.talon_BR;
+		pigeon = RobotMap.pigeon;
+		differentialDrive = new DifferentialDrive(talon_FL, talon_FR);
 
-  public void tankDrive(double leftValue, double rightValue) {
-    differentialDrive.tankDrive(leftValue, rightValue);
-  }
+		talon_BL.follow(talon_FL);
+		talon_BR.follow(talon_FR);
+	}
 
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-  }
+	public void tankDrive(double leftValue, double rightValue) {
+		differentialDrive.tankDrive(leftValue, rightValue);
+	}
+
+	@Override
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 
 }
