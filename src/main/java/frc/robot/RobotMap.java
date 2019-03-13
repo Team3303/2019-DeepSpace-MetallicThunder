d1ab26e0529eb8253a6fe56661a837346e03167b
Madsen Altamirano <1supersonicfan@gmail.com>
2019-03-12 18:27:00 -0700

    Rearrange RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -19,7 +19,8 @@ public class RobotMap {
 	public static final int TALON_FR_ID = 3;
 	public static final int TALON_BR_ID = 4;
 	public static final int TALON_BI_ID = 5;
-	public static final int TALON_EB_ID = 7;
+	public static final int TALON_EL_ID = 7;
+	public static final int TALON_BROT_ID = 6;
 
 //	public static WPI_VictorSPX talon_FL;
 //	public static WPI_VictorSPX talon_FR;
@@ -27,47 +28,50 @@ public class RobotMap {
 	public static final int SPARK_BIL_ID = 1;
 	public static final int SPARK_BIR_ID = 2;
 
-	public static final int TALON_BROT_ID = 6;
 
 	public static WPI_TalonSRX talon_FL;
 	public static WPI_TalonSRX talon_FR;
-
 	public static WPI_TalonSRX talon_BL;
 	public static WPI_TalonSRX talon_BR;
 	public static WPI_TalonSRX talon_BI;
 	public static WPI_TalonSRX talon_EL;
-	public static WPI_TalonSRX talon_BIL;
-	public static PigeonIMU pigeon;
-	public static Spark spark_BIL;
+	public static WPI_TalonSRX ballRotator;
 
+	public static Spark spark_BIL;
 	public static Spark spark_BIR;
 
 	public static DoubleSolenoid clawSolenoid;
 	public static DoubleSolenoid clawInAndOut;
+
+	public static PigeonIMU pigeon;
 	public static Encoder encoder;
 
-	public static WPI_TalonSRX ballRotator;
 
 	public static void init() {
+		// Motorcontrollers
 //		talon_FL = new WPI_VictorSPX(RobotMap.TALON_FL_ID);
 //		talon_FR = new WPI_VictorSPX(RobotMap.TALON_FR_ID);
 		talon_FL = new WPI_TalonSRX(RobotMap.TALON_FL_ID);
 		talon_FR = new WPI_TalonSRX(RobotMap.TALON_FR_ID);
 		talon_BL = new WPI_TalonSRX(RobotMap.TALON_BL_ID);
 		talon_BR = new WPI_TalonSRX(RobotMap.TALON_BR_ID);
+		talon_EL = new WPI_TalonSRX(RobotMap.TALON_EL_ID);
+		ballRotator = new WPI_TalonSRX(RobotMap.TALON_BROT_ID);
+
 		if (Robot.isCompRobot) {
 			talon_BI = new WPI_TalonSRX(RobotMap.TALON_BI_ID);
 		} else {
 			spark_BIR = new Spark(RobotMap.SPARK_BIR_ID);
 			spark_BIL = new Spark(RobotMap.SPARK_BIL_ID);
 		}
-		talon_EL = new WPI_TalonSRX(TALON_EB_ID);
+
+		// Solenoids
 		clawSolenoid = new DoubleSolenoid(0, 1);
 		clawInAndOut = new DoubleSolenoid(2, 3);
-		pigeon = new PigeonIMU(0);
 
-		ballRotator = new WPI_TalonSRX(RobotMap.TALON_BROT_ID);
-		//encoder = new Encoder();
+		// Sensors
+		pigeon = new PigeonIMU(0);
+		encoder = new Encoder(0,1);
 	}
 
 	public static void resetControllers() {
